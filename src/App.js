4c0c9fdb96846e338fed648745a3351e6830a952
J3SS13 <jessicamclaughlan@me.com>
2018-11-14 20:59:23 -0500

    working on onClick- currently not functioning as I would like

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import RandomizeBirds from './components/BirdPages/BirdOptions.js'
+import SelectedBird from './components/BirdPages/SelectedBird.js'
 import Welcome from './components/Welcome.js'
 import birds from './services/birds.js'
 import axios from 'axios';
@@ -19,6 +20,7 @@ class App extends Component {
     };
       this.setView = this.setView.bind(this);
       this.createBirds = this.createBirds.bind(this);
+      this.createBirdInfo = this.createBirdInfo.bind(this);
   }
 
   getView(){
@@ -27,7 +29,7 @@ class App extends Component {
       case 'welcome':
         return <Welcome setView={this.setView}/>
       case 'birds':
-        return <RandomizeBirds showInfo={this.state.selectedBird} birds={this.state.birds} createBirds={this.createBirds}/>
+        return <RandomizeBirds showInfo={this.state.selectedBird} birds={this.state.birds} createBirds={this.createBirds} createBirdInfo={this.createBirdInfo}/>
       default :
         return <Welcome />
     }
@@ -36,14 +38,19 @@ class App extends Component {
   createBirds(){
     return this.state.birds.map(bird => {
       return (
-        <div onClick={()=>this.setState({
-          selectedName: bird.name,
-          selectedDesc: bird.description,
-          selectedSound: ''
-        })} key={bird.id}> {bird.name}</div>
+        <div onClick={()=>this.handleSelect(bird)} key={bird.id}> {bird.name}</div>
       )
     })
   }
+
+createBirdInfo(){
+  console.log("this ran");
+  return (
+    <div className="info" key="info"> <h1> {this.state.selectedName} </h1> <p> {this.state.selectedDesc} </p> </div>
+  )
+}
+
+
 
   setView(view){
     this.setState({
@@ -51,11 +58,13 @@ class App extends Component {
     });
   }
 
-  // handleSelect(bird){
-  //   this.setState({
-  //     selectedBird: bird.name
-  //   });
-  // }
+  handleSelect(bird){
+    this.setState({
+      selectedName: bird.name,
+      selectedDesc: bird.description,
+      selectedSound: ''
+    });
+  }
 
   render() {
     return (
--- src/components/BirdPages/BirdOptions.js
+++ src/components/BirdPages/BirdOptions.js
@@ -12,18 +12,10 @@ function getRandomInt(min, max) {
 
 function RandomizeBirds(props){
 
-  // let num1 = getRandomInt(1, props.birds.length);
-  // let num2 = getRandomInt(1, props.birds.length);
-  // let num3 = getRandomInt(1, props.birds.length);
-  //
-
-
-
-
 return (
   <div>
       {props.createBirds()}
-      {props.selectedBirds && <SelectedBird />}
+      {props.selectedName && props.createBirdInfo()}
 
     </div>
   )
